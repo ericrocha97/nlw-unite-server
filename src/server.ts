3b1fa865ba914d3d434e55d3b1ef2257498f0983
Eric <ericrochaeric@gmail.com>
2024-04-06 01:45:45 -0300

    :bug: remove comments

--- src/server.ts
+++ src/server.ts
@@ -24,20 +24,6 @@ app.register(fastifyCors, {
     origin: '*',
 })
 
-// app.register(fastifySwagger, {
-//     swagger: {
-//         consumes: ['application/json'],
-//         produces: ['application/json'],
-//         info: {
-//             title: 'pass.in',
-//             description:
-//                 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
-//             version: '1.0.0',
-//         },
-//     },
-//     transform: jsonSchemaTransform,
-// })
-
 app.register(fastifySwagger, {
     openapi: {
         openapi: '3.0.0',
